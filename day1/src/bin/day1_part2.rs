57f9c92ce564c922219e251b83ceb8f0fcf51c48
Andrii Rubtsov <andrii.rubtsov@gmail.com>
2023-03-11 01:16:02 +0200

    Day1 / Part2

---
+++ day1/src/bin/day1_part2.rs
@@ -0,0 +1,29 @@
+/*! See https://adventofcode.com/2022/day/1 */
+
+fn sum_calories_by_top(top: usize) -> Result<u32, Box<dyn std::error::Error>> {
+    let input = std::fs::read_to_string(utils::find_empirically("day1/input.txt"))?;
+    let mut calories: Vec<u32> = input
+        .split("\n\n")
+        .map(|block| block.lines().map(|s| s.parse::<u32>().unwrap()).sum())
+        .collect();
+
+    calories.sort_by(|v1, v2| v2.cmp(v1));
+
+    Ok(calories.iter().take(top).sum::<u32>())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let sum_calories_by_top: u32 = sum_calories_by_top(3)?;
+    println!("Total calories by top 3: {}", sum_calories_by_top);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ok() {
+        assert_eq!(sum_calories_by_top(3).unwrap(), 212117);
+    }
+}
