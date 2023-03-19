c2834159f6d5b694c51396e4d93e62bf9c036b04
Andrii Rubtsov <andrii.rubtsov@gmail.com>
2023-03-19 18:40:33 +0200

    Day9: move common to `lib.rs`

--- day9/src/bin/day9_part1.rs
+++ day9/src/bin/day9_part1.rs
@@ -1,84 +1,24 @@
 /*! See https://adventofcode.com/2022/day/9 */
 
+use day9::*;
+use rust_embed::RustEmbed;
 use std::{
     collections::HashSet,
     io::{self, BufRead, BufReader, Read},
 };
 
-use rust_embed::RustEmbed;
-
 #[derive(RustEmbed)]
 #[folder = "."]
 struct Asset;
 
-#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone, Default)]
-struct Pos {
-    x: i32,
-    y: i32,
-}
-
-impl Pos {
-    pub fn new(x: i32, y: i32) -> Self {
-        Pos { x, y }
-    }
-
-    pub fn dist(&self, other: &Pos) -> usize {
-        i32::max(i32::abs(self.x - other.x), i32::abs(self.y - other.y)) as usize
-    }
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
-impl From<&str> for Direction {
-    fn from(value: &str) -> Self {
-        use Direction::*;
-
-        if value.len() != 1 {
-            panic!("Value was expected to be a single ASCII char, but was {value}")
-        }
-
-        match value.chars().next().unwrap() {
-            'U' => Up,
-            'D' => Down,
-            'L' => Left,
-            'R' => Right,
-            _ => panic!("Unsupported direction"),
-        }
-    }
-}
-
-pub struct Movement {
-    direction: Direction,
-    steps: usize,
-}
-
-impl From<&str> for Movement {
-    fn from(value: &str) -> Self {
-        let parts: Vec<_> = value.split_ascii_whitespace().collect();
-        if parts.len() != 2 {
-            panic!("Movement was expected to be in form `direction` `steps`, but was {value}")
-        }
-
-        let direction: Direction = parts[0].into();
-        let steps: usize = parts[1].parse().unwrap();
-        Movement { direction, steps }
-    }
-}
-
 #[derive(Debug)]
-pub struct BridgeMotions {
+struct SimpleRopeMotions {
     tail_positions: HashSet<Pos>,
     head: Pos,
     tail: Pos,
 }
 
-impl Default for BridgeMotions {
+impl Default for SimpleRopeMotions {
     fn default() -> Self {
         let initial_pos = Pos::default();
         let mut tail_positions = HashSet::new();
@@ -91,7 +31,7 @@ impl Default for BridgeMotions {
     }
 }
 
-impl BridgeMotions {
+impl SimpleRopeMotions {
     pub fn process_head_movement(&mut self, head_movement: &Movement) {
         use Direction::*;
 
@@ -115,12 +55,12 @@ impl BridgeMotions {
 }
 
 fn total_unique_tail_locations(reader: impl Read) -> Result<usize, io::Error> {
-    let mut bridge_motions = BridgeMotions::default();
+    let mut rope_motions = SimpleRopeMotions::default();
     for line in BufReader::new(reader).lines() {
         let movement: Movement = line?.as_str().into();
-        bridge_motions.process_head_movement(&movement);
+        rope_motions.process_head_movement(&movement);
     }
-    Ok(bridge_motions.tail_positions.len())
+    Ok(rope_motions.tail_positions.len())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
--- day9/src/lib.rs
+++ day9/src/lib.rs
@@ -1,1 +1,59 @@
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone, Default)]
+pub struct Pos {
+    pub x: i32,
+    pub y: i32,
+}
 
+impl Pos {
+    pub fn new(x: i32, y: i32) -> Self {
+        Pos { x, y }
+    }
+
+    pub fn dist(&self, other: &Pos) -> usize {
+        i32::max(i32::abs(self.x - other.x), i32::abs(self.y - other.y)) as usize
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl From<&str> for Direction {
+    fn from(value: &str) -> Self {
+        use Direction::*;
+
+        if value.len() != 1 {
+            panic!("Value was expected to be a single ASCII char, but was {value}")
+        }
+
+        match value.chars().next().unwrap() {
+            'U' => Up,
+            'D' => Down,
+            'L' => Left,
+            'R' => Right,
+            _ => panic!("Unsupported direction"),
+        }
+    }
+}
+
+pub struct Movement {
+    pub direction: Direction,
+    pub steps: usize,
+}
+
+impl From<&str> for Movement {
+    fn from(value: &str) -> Self {
+        let parts: Vec<_> = value.split_ascii_whitespace().collect();
+        if parts.len() != 2 {
+            panic!("Movement was expected to be in form `direction` `steps`, but was {value}")
+        }
+
+        let direction: Direction = parts[0].into();
+        let steps: usize = parts[1].parse().unwrap();
+        Movement { direction, steps }
+    }
+}
